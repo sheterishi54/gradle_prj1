dabb8e4c0f8792656147eaf33d56d053b20297f7
unknown <sheterishikesh5@gmail.com>
2021-08-06 10:31:29 +0530

    gradle_prj1

---
+++ src/main/java/git/SumApp1.java
@@ -0,0 +1,17 @@
+package git;
+
+public class SumApp1 {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum=0;
+		for (int i = 0; i <=10; i++) {
+			sum=sum+i;
+			
+		}
+		System.out.println("sum is"+sum);
+
+	}
+
+}
